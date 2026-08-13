a2ee52df9b6614169e7a4680d5818a2adf022d72
lucas S.M <lucas.marinho.ceu@gmail.com>
2026-08-13 19:09:54 -0300

    Tela Login terminada

--- ChamaJussa/scr/App.jsx
+++ ChamaJussa/scr/App.jsx
@@ -1,6 +1,7 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
 import { useFonts, Montserrat_700Bold, Montserrat_300Light } from "@expo-google-fonts/montserrat";
 import { styles } from "./Styles";
+import { Image } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,16 +14,28 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
+    <View style={styles.container} >
+      <Image source={require("../assets/logo.png")} style={styles.appImage}/>
       <View style={styles.loginBox}>
         <View style={styles.titleBox}>
-        <Text style={styles.title}>Chama Jussa</Text>
-        <Text style={styles.subTitle}>Gereciamento de ordens de serviço</Text>
+          <Text style={styles.title}>Chama Jussa</Text>
+          <Text style={styles.subTitle}>Gereciamento de ordens de serviço</Text>
         </View>
-        <View style={styles.inputBox}>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputBox}>
+            <Text style={styles.text}>E-mail</Text>
+            <TextInput style={styles.inputText} placeholder="E-mail"/>
+          </View>
+          <View style={styles.inputBox}>
+            <Text style={styles.text}>Senha</Text>
+            <TextInput style={styles.inputText} placeholder="Senha"/>
+          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Acessar o sistema</Text>
+          </TouchableOpacity>
         </View>
       </View>
-      
+
     </View>
   );
 }
--- ChamaJussa/scr/Styles.js
+++ ChamaJussa/scr/Styles.js
@@ -2,37 +2,87 @@ import { StyleSheet } from "react-native";
 
 
 export const styles = StyleSheet.create({
+  appImage: {
+   height: 200,
+   width: 200
+  },
   container: {
     flex: 1,
     backgroundColor: '#F3F4F6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   loginBox: {
     backgroundColor: "white",
-    height: 328,
+    height: 340,
     width: "80%",
-    borderRadius: 24,
+    borderRadius: 5,
 
-    
+
     paddingTop: 30,
     padding: 10,
+    gap: 20
   },
+
   title: {
-   fontFamily: "Montserrat_700Bold",
-   fontSize: 21,
+    fontFamily: "Montserrat_700Bold",
+    fontSize: 21,
   },
+
   subTitle: {
-   fontFamily: "Montserrat_300Light",
-   fontSize: 13,
-   fontWeight: 400,
-   color: "#868585"
+    fontFamily: "Montserrat_300Light",
+    fontSize: 13,
+    fontWeight: 400,
+    color: "#868585"
   },
+
   titleBox: {
     alignItems: "center",
     gap: 10
   },
+
+  inputContainer: {
+    alignItems: "center",
+    gap: 20
+  },
+
+  inputText: {
+    backgroundColor: "#F3F4F6",
+    height: 35,
+    width: "100%",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#0000001f",
+    borderRadius: 3,
+    padding: 10,
+    outlineColor: "#0000002f",
+    color: "#4b4b4b"
+  },
+
+  text: {
+    fontFamily: "Montserrat_700Bold",
+    fontSize: 14
+  },
+
   inputBox: {
-    
-  }     
+    width: "90%",
+    gap: 7,
+  },
+
+  button: {
+    backgroundColor: "#10B981",
+    height: 35,
+    width: "90%",
+    borderRadius: 3,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 5
+  },
+
+  buttonText: {
+    color: "white",
+    fontFamily: "Montserrat_700Bold",
+  }
 });
